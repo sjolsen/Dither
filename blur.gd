2147cf564e27e5990f7aa896e192ed5e247bc731
Stuart Olsen <stuart@sj-olsen.com>
2023-03-20 02:44:20 -0500

    Share weights between blur passes

--- blur.gd
+++ blur.gd
@@ -12,10 +12,26 @@ signal changed;
 		sigma = x
 		changed.emit()
 
-func _update_uniforms() -> void:
-	$Horizontal._update_uniforms(enable, sigma)
-	$Vertical._update_uniforms(enable, sigma)
-
 func _ready() -> void:
 	changed.connect(_update_uniforms)
 	changed.emit()
+
+func _update_uniforms() -> void:
+	var weights := _compute_weights(sigma)
+	$Horizontal.update_uniforms(enable, weights)
+	$Vertical.update_uniforms(enable, weights)
+
+func _compute_weights(sigma: float) -> Array[float]:
+	var n_weights := ceili(3.0 * sigma)
+	var weights := [] as Array[float]
+	weights.resize(n_weights)
+	var total := _gaussian(0)
+	for i in range(n_weights):
+		weights[i] = _gaussian(float(1 + i) / sigma)
+		total += 2.0 * weights[i]
+	for i in range(n_weights):
+		weights[i] /= total
+	return weights
+	
+func _gaussian(x: float) -> float:
+	return exp(-(x*x) / 2.0)
