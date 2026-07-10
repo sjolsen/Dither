0c7785cc3a8a1b573fe6e7985b978f70ebf39ed1
sjolsen <3344567+sjolsen@users.noreply.github.com>
2026-07-10 14:03:01 -0500

    Remove blur

--- blur_pass.gd
+++
@@ -1,6 +0,0 @@
-extends ColorRect
-
-func update_uniforms(enable: bool, weights: Array[float]) -> void:
-	material.set_shader_parameter("enable", enable)
-	material.set_shader_parameter("weights", weights)
-	material.set_shader_parameter("n_weights", weights.size())
