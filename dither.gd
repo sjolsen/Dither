92dba89e37bfecb19389cd90b8298068a85ce99b
sjolsen <3344567+sjolsen@users.noreply.github.com>
2026-07-11 03:04:26 -0500

    Replace the fragment shader with the skeleton of a compute shader

---
+++ dither.gd
@@ -0,0 +1,60 @@
+@tool
+extends CompositorEffect
+class_name Dither
+
+
+var _rd := RenderingServer.get_rendering_device()
+var _shader_file: RDShaderFile = load("res://dither.glsl")
+var _shader := RID()
+var _pipeline := RID()
+
+
+func _init() -> void:
+	effect_callback_type = CompositorEffect.EFFECT_CALLBACK_TYPE_POST_TRANSPARENT
+	_shader_file.changed.connect(_reload_shader)
+	_reload_shader()
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_PREDELETE:
+		if _shader:
+			_rd.free_rid(_shader)
+
+
+func _reload_shader() -> void:
+	if _shader:
+		_rd.free_rid(_shader)
+
+	var spirv := _shader_file.get_spirv()
+	if spirv.compile_error_compute:
+		push_error(spirv.compile_error_compute)
+		assert(false)
+
+	_shader = _rd.shader_create_from_spirv(spirv)
+	_pipeline = _rd.compute_pipeline_create(_shader)
+
+
+func _render_callback(_callback_type: int, render_data: RenderData) -> void:
+	var buffers: RenderSceneBuffersRD = render_data.get_render_scene_buffers()
+
+	var size: Vector2i = buffers.get_internal_size()
+	var x_groups := (size.x - 1) / 8 + 1
+	var y_groups := (size.y - 1) / 8 + 1
+	var push_constant := PackedFloat32Array([size.x, size.y, 0.0, 0.0])
+
+	for view in range(buffers.get_view_count()):
+		var image: RID = buffers.get_color_layer(view)
+
+		var uniform := RDUniform.new()
+		uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+		uniform.binding = 0
+		uniform.add_id(image)
+		var uniform_set: RID = UniformSetCacheRD.get_cache(_shader, 0, [uniform])
+
+		var compute_list := _rd.compute_list_begin()
+		_rd.compute_list_bind_compute_pipeline(compute_list, _pipeline)
+		_rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+		var pc := push_constant.to_byte_array()
+		_rd.compute_list_set_push_constant(compute_list, pc, pc.size())
+		_rd.compute_list_dispatch(compute_list, x_groups, y_groups, 1)
+		_rd.compute_list_end()
